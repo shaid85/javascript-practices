3be1c02c80c62525d9d16bd897cd378f26085630
Shaid Islam <shaid85@gmail.com>
2022-03-26 15:51:14 +0600

    getAnyUpdate function added

---
+++ todo.js
@@ -0,0 +1,16 @@
+function getAnyUpdate(){
+    title = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+
+    if(localStorage.getItem('itemsJson') == null){
+        itemJsonArray = [];
+        itemJsonArray.push([title, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    } else {
+        itemJsonArrayStr = localStorage.getItem('itemsJson');
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([title, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+
+    }
+}
