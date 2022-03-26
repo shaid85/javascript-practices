b2288db224547e194125ce720dd4a43415559d6a
Shaid Islam <shaid85@gmail.com>
2022-03-26 14:21:44 +0600

    replace DOM text by ReplaceChild

--- practices.js
+++ practices.js
@@ -8,3 +8,8 @@ const newDiv = document.getElementsByTagName('div');
 let createdElement = document.createElement('p');
 createdElement.innerText = "This is new Dom Text";
 newDiv[0].appendChild(createdElement);
+
+let createdElement2 = document.createElement('b');
+createdElement2.innerText = "This is new DOM Bold Text";
+newDiv[0].replaceChild(createdElement2,createdElement);
+
